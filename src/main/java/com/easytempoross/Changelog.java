60da6409ca5f2a57e51ac233c4552e7d17bebe0d
Jam <1347620+JamsRepos@users.noreply.github.com>
2026-09-01 19:39:21 +0100

    Improve Plugin Hub description without a version bump.

--- src/main/java/com/easytempoross/Changelog.java
+++ src/main/java/com/easytempoross/Changelog.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 final class Changelog
 {
-	static final String VERSION = "1.0.4";
+	static final String VERSION = "1.0.2";
 
 	static final List<Release> RELEASES = List.of(
 		new Release("1.0.0",
@@ -19,9 +19,6 @@ final class Changelog
 		),
 		new Release("1.0.2",
 			"Renamed from Easy Tempoross to Jam's Solo Tempoross in the plugin panel and Hub."
-		),
-		new Release("1.0.4",
-			"Improved Plugin Hub description."
 		)
 	);
 
